eed8888899649e2a41c30bb932e76ddbeaa25922
Vinay Pai <vinay@vinaypai.com>
2021-03-08 00:12:41 -0500

    Fix build issues and add note

--- setup.py
+++ setup.py
@@ -1,18 +1,21 @@
 import setuptools
 
+# python3 -m build --sdist
+# python3 -m twine upload dist/*
+
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
     name='django-transactional-mail',
-    version='0.2.2',
+    version='0.2.3',
     author='Vinay Pai',
     author_email='vinay@vinaypai.com',
     description='Transactional emails in Django projects made easy',
     long_description=long_description,
     long_description_content_type="text/markdown",
     url='https://github.com/vinaypai/django-transactional-mail/',
-    packages=['transactional_mail'],
+    packages=setuptools.find_packages(include=['transactional_mail', 'transactional_mail.*']),
     include_package_data=True,
     classifiers=[
         'Development Status :: 3 - Alpha',
@@ -31,3 +34,4 @@ setuptools.setup(
         'cssutils >= 1.0.2'
     ]
 )
+
